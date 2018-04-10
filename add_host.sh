8718f85d1307152efff1a00302e186250f94d709
rouckyhood <38083754+rouckyhood@users.noreply.github.com>
2018-04-10 09:56:45 +0200

    Update add_host.sh

--- add_host.sh
+++ add_host.sh
@@ -12,7 +12,44 @@ define host{
     address         $ip
     realm           $nomentite
 }
+define service {
+    service_description     CPU Usage
+        host_name       $nom-$nomentite
+    check_command           check_ncpa!-t 'zenadmin' -P 5693 -M cpu/percent -w 80 -c 90 -q 'aggregate=avg'
+    max_check_attempts      5
+    check_interval          5
+    retry_interval          1
+    check_period            24x7
+    notification_interval   60
+    notification_period     24x7
+    register                1
+}
 
+define service {
+    service_description     Memory Usage
+        host_name       $nom-$nomentite
+    check_command           check_ncpa!-t 'zenadmin' -P 5693 -M memory/virtual -w 80 -c 90 -u G
+    max_check_attempts      5
+    check_interval          5
+    retry_interval          1
+    check_period            24x7
+    notification_interval   60
+    notification_period     24x7
+     register                1
+}
+
+define service {
+    service_description     Disk Usage
+        host_name       $nom-$nomentite
+    check_command           check_ncpa!-t 'zenadmin' -M 'disk/logical/C:|/free' --warning 25: --critical 10: -u G
+    max_check_attempts      5
+    check_interval          5
+    retry_interval          1
+    check_period            24x7
+    notification_interval   60
+    notification_period     24x7
+    register                1
+}
 EOF
 
 sudo systemctl restart shinken
